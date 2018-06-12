8c8f7cf5309a1e99738d9c6d6dd89887a8e1af66
Horckun <horckun@gmail.com>
2018-06-12 19:34:36 +0200

    12/06/18
    Nuevo ejercicio explicado

---
+++ Ejercicios explicados/Primer programa.c
@@ -0,0 +1,18 @@
+// Primer programa
+
+// Librerias
+#include <stdio.h>
+#include <stdlib.h>
+
+// Funcion principal
+int main(void) {
+	
+	// Mensaje en pantalla
+	printf("Mi primer programa.\n");
+	
+	// Hacemos una pausa
+	printf("Presiona Intro para finalizar...");
+	getchar();
+	
+	return 0;
+}
