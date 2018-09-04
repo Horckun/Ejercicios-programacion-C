8ae5f398a0de4fa426c3260609475374c6c5c767
Horckun <horckun@gmail.com>
2018-09-04 20:45:40 +0200

    04/09/18
    IF ELSE

--- Ejercicios explicados/022 - If else.c
+++ Ejercicios explicados/022 - If else.c
@@ -1,4 +1,4 @@
-// IF
+// IF ELSE
 
 // LIBRERÍA
 #include <stdio.h>
@@ -16,10 +16,14 @@ int main() {
 	// CONDICIONAL IF
 	if(x >= 0) {
 		printf("El valor es positivo.\n");
+	} else {
+		printf("El valor es negativo.\n");
 	}
 	
 	if(x >= 0 && x <= 9) {
 		printf("El valor tiene un digito.\n");
+	} else {
+		printf("El valor tiene mas de un digito.\n");
 	}
 	
 	// CIERRE
