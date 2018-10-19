90349aff240a50bd80048d9dc9e71fb5d7b455e7
Horckun <horckun@gmail.com>
2018-10-19 18:42:46 +0200

    040
    Estructura

---
+++ Ejercicios explicados/040 - Estructura.c
@@ -0,0 +1,21 @@
+// Estructura
+
+// Librería
+#include <stdio.h>
+
+// Estructura
+struct ficha {
+	char nombre[20];
+	int edad;
+	int nota1;
+	int nota2;
+	int nota3;
+	int notamedia;
+} fichas;
+
+// Función principal
+int main() {
+	
+	// Cierre
+	return 0;
+}
