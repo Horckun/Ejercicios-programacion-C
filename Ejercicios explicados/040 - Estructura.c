995733430e38fc655423ba8442ec135fa1291e36
Horckun <horckun@gmail.com>
2018-10-22 14:44:31 +0200

    update
    040

--- Ejercicios explicados/040 - Estructura.c
+++ Ejercicios explicados/040 - Estructura.c
@@ -4,17 +4,24 @@
 #include <stdio.h>
 
 // Estructura
-struct ficha {
+struct info {
 	char nombre[20];
 	int edad;
-	int nota1;
-	int nota2;
-	int nota3;
-	int notamedia;
-} fichas;
+} ficha;
 
 // Función principal
 int main() {
+	// Pedimos nombre y edad
+	printf("Introduce el nombre: ");
+	fflush(stdin);
+	gets(ficha.nombre);
+	printf("Ahora introduce la edad: ");
+	scanf("%d",&ficha.edad);
+	printf("\n");
+	
+	// Mostramos nombre y edad
+	printf("Nombre: %s.\n",ficha.nombre);
+	printf("Edad: %d a%cos.\n\n",ficha.edad,164);
 	
 	// Cierre
 	return 0;
