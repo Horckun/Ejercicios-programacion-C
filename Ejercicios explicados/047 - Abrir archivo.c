0f76ae501c88acbedb3ab27df6ff2ab2d3ea373e
Horckun <horckun@gmail.com>
2018-11-05 16:07:31 +0100

    047
    Abrir archivo

---
+++ Ejercicios explicados/047 - Abrir archivo.c
@@ -0,0 +1,22 @@
+// Abrir archivo
+
+// Librería
+#include <stdio.h>
+
+// Función principal
+int main() {
+	// Archivo *variable
+	FILE *archivo;
+	// Variable = abrir archivo
+	archivo = fopen("archivo.txt","rt");
+	// Comprobar si se abre
+	if(archivo == NULL) {
+		printf("No se ha podido abrir el archivo.\n\n");
+	}
+	
+	// Cierre
+	printf("Pulsa una tecla para finalizar...");
+	fflush(stdin);
+	getch();
+	return 0;
+}
