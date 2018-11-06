f57daa48a2837e18d93851b4ea1d78741316fce6
Horckun <horckun@gmail.com>
2018-11-06 18:35:18 +0100

    Update
    Cerrar archivo

---
+++ Ejercicios explicados/048 - Cerrar archivo.c
@@ -0,0 +1,25 @@
+// Cerrar archivo
+
+// Librería
+#include <stdio.h>
+
+// Función principal
+int main() {
+	// Archivo *variable
+	FILE *archivo;
+	// Variable = abrir archivo
+	archivo = fopen("archivo.txt","rt");
+	// Comprobar si se abre
+	if(archivo == NULL) {
+		printf("No se ha podido abrir el archivo.\n\n");
+	}
+	
+	// Cerrar archivo
+	fclose(archivo);
+	
+	// Cierre
+	printf("Pulsa una tecla para finalizar...");
+	fflush(stdin);
+	getch();
+	return 0;
+}
